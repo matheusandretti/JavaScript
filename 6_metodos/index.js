b24b7649bde698e95509d66a1a9100ff90d42646
Matheus Andretti <andrettiguermo@gmail.com>
2024-09-18 12:56:00 -0300

    6

---
+++ 6_metodos/index.js
@@ -0,0 +1,12 @@
+let pessoa = {
+    nome: "Matheus",
+    falar: function(){
+        console.log("Oi, meu nome é Matheus");
+    }
+}
+
+console.log(pessoa);
+
+pessoa.falar();
+
+console.log(pessoa.falar())
