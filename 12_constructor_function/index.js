7366dc8ec3255dd537e8dcdb068596f39cc149f9
Matheus Andretti <andrettiguermo@gmail.com>
2024-09-18 20:50:24 -0300

    Update 1809

--- 12_constructor_function/index.js
+++ 12_constructor_function/index.js
@@ -2,15 +2,19 @@ function Ninja(nome, arma) {
     this.nome = nome;
     this.arma = arma;
     this.atirarArma = function(){
-        console.log("Atirou a " + this.arma)
-    }
+        console.log("Atirou a " + this.arma);
+    };
     
 }
 
-let ninja1 = new ninja('boruto', 'shuriken');
+let ninja1 = new Ninja('Naruto', 'shuriken');
 
-console.log(ninja.nome);
+console.log(ninja1.nome);
 
 ninja1.atirarArma();
 
-let ninja2 = new Ninja('Sasuke', 'kunai9')
+let ninja2 = new Ninja('Sasuke', 'kunai');
+
+console.log(ninja2.nome);
+
+ninja2.atirarArma();
