12aa01ce4364af83f5260a3c715ed1ddb09bbc5a
Matheus Andretti <andrettiguermo@gmail.com>
2024-09-17 21:14:21 -0300

    Update

---
+++ 4_prop_multi_palavra/index.js
@@ -0,0 +1,9 @@
+let cavalo = {
+    patas: 4,
+    "está domesticado": false,
+    estaDomesticado: false
+}
+
+console.log(cavalo);
+console.log(cavalo["está domesticado"]);
+console.log(cavalo.estaDomesticado);
